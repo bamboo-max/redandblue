fee1085b9c8abdb39371dc35d9f4b8050ab963e0
bamboo <huangshan@163.com>
2024-12-26 14:39:47 +0800

    修改

--- inputinfo.py
+++ inputinfo.py
@@ -2,5 +2,5 @@
 red1 = input("输入红球1：")
 print("红球1：",int(red1))
 
-arrr1[] = input("请输入两个红球号，用‘,’号隔开：").split(",")
-print(f"a={arrr1}")
+a,b = input("请输入两个红球号，用‘,’号隔开：").split(",")
+print(f"a={a},b={b}")
