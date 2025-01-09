0486d296d0ab49c4b86f617d29e574225ddd0ad8
bamboo <huangshan@163.com>
2025-01-09 14:47:55 +0800

    修改

--- pro2.py
+++ pro2.py
@@ -4,10 +4,10 @@ import random
 
 
 
-maxSum = 1996578
-#maxSum = random.randint(2000000,10000000)
+#maxSum = 965985
+maxSum = random.randint(1000000,10000000)
 s = 0
-random.seed(2025003)
+random.seed(20250109)
 red_arr = [0] * 6 ##存储生产的随机数组
 while s < maxSum:
     red_arr = [0] * 6 ##存储生产的随机数组
